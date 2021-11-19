e3a519eb0b059416bcd602b61d5f5adac6e3d4b0
pjnalls <pjnalls.js@gmail.com>
2021-11-19 17:45:19 -0600

    `ng new` app and add main title screen.

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TitleComponent } from './components/main/title/title.component';
+import { ButtonComponent } from './components/main/button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TitleComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
