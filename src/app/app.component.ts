36594087e6c3df9d840337f613ec98e0595eec51
pjnalls <pjnalls.js@gmail.com>
2021-11-21 06:19:25 -0600

    Reinforce black background overlay.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,19 @@ export class AppComponent {
   title = 'Big Blue Sky';
   main__btnText = 'Open Menu';
   openMenuBtnClicked = false;
+  toggleBlackBackground = false;
 
   onClick() {
     this.openMenuBtnClicked = !this.openMenuBtnClicked;
     this.main__btnText = this.openMenuBtnClicked ? 'Close Menu' : 'Open Menu';
+
+    if (this.toggleBlackBackground) {
+      this.toggleBlackBackground = !this.toggleBlackBackground;
+    } else {
+      setTimeout(
+        () => (this.toggleBlackBackground = !this.toggleBlackBackground),
+        1000
+      );
+    }
   }
 }
