ab6684bc5cd2eff94b8fab861d628a650eba3a22
pjnalls <pjnalls.js@gmail.com>
2021-11-21 04:31:53 -0600

    `ng g c` `MenuItemViewerComponent`.

---
+++ src/app/components/menu/menu-item-viewer/menu-item-viewer.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuItemViewerComponent } from './menu-item-viewer.component';
+
+describe('MenuItemViewerComponent', () => {
+  let component: MenuItemViewerComponent;
+  let fixture: ComponentFixture<MenuItemViewerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MenuItemViewerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MenuItemViewerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/menu/menu-item-viewer/menu-item-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-item-viewer',
+  templateUrl: './menu-item-viewer.component.html',
+  styleUrls: ['./menu-item-viewer.component.scss']
+})
+export class MenuItemViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
