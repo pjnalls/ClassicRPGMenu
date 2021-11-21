27d3284cfe86c4b6033c46efa3d30ed700849a4d
pjnalls <pjnalls.js@gmail.com>
2021-11-21 09:42:30 -0600

    Add `menuItem` `@Input()`.

--- src/app/components/menu/menu-item/menu-item.component.ts
+++ src/app/components/menu/menu-item/menu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-item',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-item.component.scss']
 })
 export class MenuItemComponent implements OnInit {
-
+  @Input() menuItem = '';
   constructor() { }
 
   ngOnInit(): void {
